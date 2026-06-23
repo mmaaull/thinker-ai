303ee2cc6745bfb4c40a0caa19a7640db8d1cb41
mmaaull <maulfaiz4@gmail.com>
2026-06-23 12:57:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>THINKER AI</h1>
+      <p>Transforming AI into Thinking Partner</p>
+    </div>
+  );
+}
+
+export default App;
